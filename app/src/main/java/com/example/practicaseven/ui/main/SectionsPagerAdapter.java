eeba8a54685d709245bc383bbd3c2a0b91ec7958
vikabur04 <vikaburtseva04@mail.ru>
2022-12-20 16:25:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/practicaseven/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.practicaseven.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.practicaseven.FirstActivity;
+import com.example.practicaseven.R;
+import com.example.practicaseven.SecondActivity;
+import com.example.practicaseven.ThirdActivity;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                FirstActivity Tab1= new FirstActivity();
+                return Tab1;
+            case 1:
+                SecondActivity Tab2= new SecondActivity();
+                return Tab2;
+            case 2:
+                ThirdActivity Tab3= new ThirdActivity();
+                return Tab3;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
